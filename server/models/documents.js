53090627166ae0a9507c28cb6e473ca90d9a2508
andela-judensi <joshua.udensi@andela.com>
2017-05-13 15:51:03 +0100

    feature(document) define document models
    define database model for document
    
    [#145389353]

---
+++ server/models/documents.js
@@ -0,0 +1,40 @@
+export default (sequelize, DataTypes) => {
+  const Documents = sequelize.define('Documents', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [10, 150],
+          msg: 'Document title must be between 10 and 150 characters',
+        },
+      },
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    access: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'public',
+      validate: {
+        isIn: [['public', 'private', 'role']],
+      },
+    },
+    ownerID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    classMethods: {
+      associate(models) {
+        Documents.belongsTo(models.Users, {
+          foreignKey: 'ownerID',
+        });
+      }
+    }
+  });
+  return Documents;
+};
