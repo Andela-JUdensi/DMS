ed6e0b8a06bccc72f1361342dd277133f12c6363
andela-judensi <joshua.udensi@andela.com>
2017-05-13 15:46:05 +0100

    test(document.js) seed file for testing document endpoint [#145389353]

---
+++ server/tests/integration/seeder/documents.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    title: 'how to read a book',
+    body: 'learn how to read a book by motimer adler',
+    ownerID: 1,
+  },
+  {
+    title: 'who moved my cheese',
+    body: 'can not even remember the author of this one',
+    ownerID: 2,
+  },
+  {
+    title: 'a place called zero',
+    body: 'definitely written by shaffin de zane',
+    ownerID: 3,
+  },
+  {
+    title: 'the naked apes',
+    body: 'by desmond elliot. My loved book in evolutionary behavior',
+    ownerID: 4,
+  },
+  {
+    title: 'the bounds of reason',
+    body: 'behavioral economics by herbert gintis, a classic',
+    ownerID: 4,
+  },
+];
