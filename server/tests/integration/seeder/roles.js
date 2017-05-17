5856015a03cdbd66734bd9ad38e7edb4c22d5527
andela-judensi <joshua.udensi@andela.com>
2017-05-17 20:07:59 +0100

    tesst(seeders): add seed files
    sedd files for test support
    
    [#145389579]

---
+++ server/tests/integration/seeder/roles.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    roleName: 'superadmin',
+    description: 'can add admins',
+  },
+  {
+    roleName: 'admin',
+    description: 'have admin priviledge',
+  },
+  {
+    roleName: 'regular',
+    description: 'do stuff like a normal person',
+  },
+];
---
+++ server/tests/integration/seeder/users.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    username: 'peterparker',
+    firstname: 'Peter',
+    lastname: 'Parker',
+    email: 'peter@email.com',
+    password: 'password123',
+    roleID: 1,
+  },
+  {
+    username: 'johndoe',
+    firstname: 'John',
+    lastname: 'Doe',
+    email: 'john@email.com',
+    password: 'password123',
+    roleID: 2,
+  },
+  {
+    username: 'janedoe',
+    firstname: 'Jane',
+    lastname: 'Doe',
+    email: 'jane@email.com',
+    password: 'password123',
+    roleID: 3,
+  },
+  {
+    username: 'irishpotato',
+    firstname: 'Irish',
+    lastname: 'Potato',
+    email: 'irish@email.com',
+    password: 'password123',
+    roleID: 3,
+  }
+];
