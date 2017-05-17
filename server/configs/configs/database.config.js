23463540a4cd1b996ec5a91346b83e9183c23ae1
andela-judensi <joshua.udensi@andela.com>
2017-05-17 21:29:27 +0100

    test(database config): database configs
    [#145389579]

--- server/configs/configs/database.config.js
+++ server/configs/configs/database.config.js
@@ -8,6 +8,7 @@ module.exports = {
     url: process.env.DEV_DATABASE_URL,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL'
   },
   test: {
     database: process.env.TEST_DB_NAME,
@@ -17,6 +18,7 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false,
+    use_env_variable: 'DATABASE_URL'
   },
   production: {
     database: process.env.PROD_DB_NAME,
@@ -25,5 +27,6 @@ module.exports = {
     url: process.env.PROD_DATABASE_URL,
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL'
   },
 };
