bcfa6578e37933b4b52e37e770a22cde9bbd5cef
andela-judensi <joshua.udensi@andela.com>
2017-05-17 20:06:15 +0100

    feature(helpers): add helpers helper classes for handling ressponses and authentication [#145389579]

---
+++ server/utils/Helpers.js
@@ -0,0 +1,105 @@
+import models from '../models';
+
+const Blacklists = models.Blacklists;
+
+export default class Helpers {
+
+  /**
+   *
+   *
+   * @static
+   * @param {any} userID
+   * @param {any} userRoleID
+   * @returns
+   *
+   * @memberOf Helpers
+   */
+  static determineDocsforUser(userID, userRoleID) {
+    if (!(userID)) {
+      return [{ access: 'public' }];
+    } else if (userRoleID <= 2) {
+      return [{ access: 'private' }, { access: 'public' }, { access: 'role' }, { ownerID: userID }];
+    }
+    return [{ access: 'public' }, { access: 'role' }, { ownerID: userID }];
+  }
+
+  /**
+   *
+   *
+   * @static
+   * @param {any} userID
+   * @param {any} roleID
+   * @returns
+   *
+   * @memberof Helpers
+   */
+  static getUserFromHeaders(userID, roleID) {
+    if (!(userID) || !(roleID)) {
+      return {
+        userID: 0,
+        roleID: 0
+      };
+    }
+    const uID = userID.split(' ')[1];
+    const rID = roleID.split(' ')[1];
+    return {
+      userID: uID,
+      roleID: rID
+    };
+  }
+
+
+  static determineSearchQuery(searchAccess, searchTokens, userID, roleID) {
+    let query = '';
+    if (searchAccess === 'undefined') {
+      query = {
+        title: {
+          $iLike: { $any: searchTokens },
+        },
+        $or: this.determineDocsforUser(userID, roleID)
+      };
+      return query;
+    }
+    query = {
+      title: {
+        $ilike: { $any: searchTokens },
+      },
+      access: searchAccess,
+      $or: this.determineDocsforUser(userID, roleID)
+    };
+    return query;
+  }
+
+  static BlacklistToken(authorizationToken) {
+    return new Promise((resolve) => {
+      Blacklists.create({
+        authorizationToken
+      })
+      .then(() => {
+        resolve(true);
+      });
+    });
+    // .then(() => )
+    // .catch(() => )
+  }
+
+  static TokenIsBlacklisted(authorizationToken) {
+    return new Promise((resolve) => {
+      if (!authorizationToken) { resolve(false); }
+      const token = authorizationToken.split(' ')[1];
+      Blacklists.findOne({
+        where: {
+          authorizationToken: token,
+        }
+      })
+      .then((foundToken) => {
+        if (!foundToken) {
+          return resolve(false);
+        }
+        return resolve(true);
+      });
+    });
+    // .catch();
+  }
+}
+
---
+++ server/utils/Response.js
@@ -0,0 +1,167 @@
+export default class Response {
+
+  /**
+   * Creates an instance of Response.
+   * 
+   * @memberOf Response
+   */
+  constructor() {
+    this.status = 200;
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @param {any} status 
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static setStatus(status) {
+    this.status = status;
+    return this;
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static getStatus() {
+    return this.status;
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @param {any} res 
+   * @param {any} data 
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static respond(res, data) {
+    return res.status(this.getStatus())
+      .json(data);
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @param {any} res 
+   * @param {any} data 
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static success(res, data) {
+    return this.setStatus(200)
+      .respond(res, data);
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @param {any} res 
+   * @param {any} message 
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static notFound(res, message) {
+    return this.setStatus(404)
+      .respond(res, {
+        message,
+      });
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @param {any} res 
+   * @param {any} message 
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static internalError(res, message) {
+    return this.setStatus(500)
+      .respond(res, {
+        message,
+      });
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @param {any} res 
+   * @param {any} message 
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static badRequest(res, message) {
+    return this.setStatus(400)
+      .respond(res, {
+        message
+      });
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @param {any} res 
+   * @param {any} message 
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static unAuthorized(res, message) {
+    return this.setStatus(401)
+      .respond(res, {
+        message
+      });
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @param {any} res 
+   * @param {any} message 
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static forbidden(res, message) {
+    return this.setStatus(403)
+      .respond(res, {
+        message
+      });
+  }
+
+  /**
+   * 
+   * 
+   * @static
+   * @param {any} res 
+   * @param {any} data 
+   * @returns 
+   * 
+   * @memberOf Response
+   */
+  static created(res, data) {
+    return this.setStatus(201)
+      .respond(res, data);
+  }
+}
