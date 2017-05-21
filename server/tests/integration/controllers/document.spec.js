426c06a66c7641735a9669008b8c9e247117c206
andela-judensi <joshua.udensi@andela.com>
2017-05-21 13:15:13 +0100

    test(document): add documents test
    [#145389353]

---
+++ server/tests/integration/controllers/document.spec.js
@@ -0,0 +1,314 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../../../server';
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('The document API', () => {
+  let authenticatedUser;
+  describe('for authenticated users', () => {
+    before((done) => {
+      chai.request(server)
+        .post('/api/users/login/')
+        .send({
+          identifier: 'SiliconValley',
+          password: 'password123',
+        })
+        .end((err, res) => {
+          authenticatedUser = res.body;
+          res.should.have.status(200);
+          done();
+        });
+    });
+    it('should return `bad request` to create with invalid records', (done) => {
+      chai.request(server)
+        .post('/api/documents/')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .send({
+          title: 'Int',
+          body: `Rene Descartes wrote “common sense is the most widely shared commodity in the world, 
+            for every man is convinced that he is well supplied with it”. We all like to think of how endowed we are 
+            with common sense. After all, it is common. If this conviction equals reality, 
+            and common sense is really a valid way of obtaining reliable results, then the 
+            world would be less troubled. For common sense is “seeing things as they are; and 
+            doing things as they ought to be”, said Harriet Beecher Stowe.`,
+          access: 'public'
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.message.should.eql('Validation error: document title must be between 5 and 100 characters');
+          done();
+        });
+    });
+    it('should return `success` creating a new document with valid records', (done) => {
+      chai.request(server)
+        .post('/api/documents/')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .send({
+          title: 'Introduction to commonsense',
+          body: `Rene Descartes wrote “common sense is the most widely shared commodity in the world, 
+            for every man is convinced that he is well supplied with it”. We all like to think of how endowed we are 
+            with common sense. After all, it is common. If this conviction equals reality, 
+            and common sense is really a valid way of obtaining reliable results, then the 
+            world would be less troubled. For common sense is “seeing things as they are; and 
+            doing things as they ought to be”, said Harriet Beecher Stowe.`,
+          access: 'public'
+        })
+        .end((err, res) => {
+          res.should.have.status(201);
+          done();
+        });
+    });
+    it('should return all documents based on user privilege', (done) => {
+      chai.request(server)
+        .get('/api/documents/')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.count.should.be.a('number');
+          res.body.rows.should.be.an('array');
+          done();
+        });
+    });
+    it('should return one document, based on parameter', (done) => {
+      chai.request(server)
+        .get('/api/documents/1')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.title.should.eql('how to read a book');
+          res.body.access.should.eql('public');
+          res.body.User.username.should.eql('SiliconValley');
+          done();
+        });
+    });
+    it('should return `not found`, if document is not found', (done) => {
+      chai.request(server)
+        .get('/api/documents/100')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.message.should.eql('document not found');
+          done();
+        });
+    });
+    it('return `bad request` in request with an invalid id', (done) => {
+      chai.request(server)
+        .get('/api/documents/abcde')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.message.should.eql('provide a valid document id');
+          done();
+        });
+    });
+    it('should return `you are not authorized` in request to update document above access level', (done) => {
+      chai.request(server)
+        .put('/api/documents/3')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .send({ title: 'A Place Called Zero' })
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.message.should.eql('you are not authorized');
+          done();
+        });
+    });
+    it('should return `document not found` in request to update document not available', (done) => {
+      chai.request(server)
+        .put('/api/documents/100')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .send({ title: 'A Discuss about Unexistence' })
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.message.should.eql('document not found');
+          done();
+        });
+    });
+    it('should return updated document', (done) => {
+      chai.request(server)
+        .put('/api/documents/1')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .send({ title: 'The Bounds of Reason - II' })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.title.should.eql('The Bounds of Reason - II');
+          done();
+        });
+    });
+    it('should return `you are not authorized` in request to delete document above access level', (done) => {
+      chai.request(server)
+        .put('/api/documents/100')
+        .set('authorization', `bearer ${authenticatedUser.token}`)
+        .set('x-userid', authenticatedUser.user.id)
+        .set('x-roleid', authenticatedUser.user.roleID)
+        .send({ title: 'A Discuss about Unexistence' })
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.message.should.eql('document not found');
+          done();
+        });
+    });
+  });
+
+  describe('for unauthenticated users', () => {
+    it('should respond with success', (done) => {
+      chai.request(server)
+        .get('/api/documents/')
+        .end((error, response) => {
+          response.body.should.be.an('object');
+          response.status.should.equal(200);
+          response.body.rows['1'].should.have.keys(['User', 'access', 'body',
+            'createdAt', 'id', 'ownerID', 'title', 'updatedAt']);
+          done();
+        });
+    });
+    it('should return only public documents without authentication', (done) => {
+      chai.request(server)
+        .get('/api/documents/')
+        .end((error, response) => {
+          response.body.should.be.an('object');
+          response.body.rows['1'].access.should.equal('public');
+          response.body.rows['2'].access.should.be.a('string');
+          response.body.rows['3'].access.should.not.equal('private');
+          response.body.rows['3'].access.should.not.equal('role');
+          done();
+        });
+    });
+    it('should return only 12 public documents without authentication by default', (done) => {
+      chai.request(server)
+        .get('/api/documents/')
+        .end((error, response) => {
+          response.body.rows.length.should.be.a('number');
+          response.body.rows.length.should.be.lte(12);
+          response.body.rows.length.should.not.be.lte(-1);
+          response.body.rows.length.should.not.be.gte(13);
+          done();
+        });
+    });
+    it('should return `you are not signed in` in attempt to create a document', (done) => {
+      chai.request(server)
+        .post('/api/documents/')
+        .send({
+          title: 'My awesome title',
+          body: 'my wonderful document content',
+          ownerID: 1,
+          access: 'public'
+        })
+        .end((error, response) => {
+          response.status.should.equal(401);
+          response.body.message.should.equal('you are not signed in');
+          done();
+        });
+    });
+    it('should return `you are not signed in` in attempt to `update` a document', (done) => {
+      chai.request(server)
+        .put('/api/documents/5')
+        .send({
+          title: 'My awesome title',
+          body: 'my wonderful document content',
+          ownerID: 1,
+          access: 'public'
+        })
+        .end((error, response) => {
+          response.status.should.equal(401);
+          response.body.message.should.equal('you are not signed in');
+          done();
+        });
+    });
+    it('should return `success` in request to a `public` document without authentication', (done) => {
+      chai.request(server)
+        .get('/api/documents/1')
+        .end((error, response) => {
+          response.status.should.equal(200);
+          // response.body.message.should.equal('document not found');
+          done();
+        });
+    });
+    it('should return `document not found` in request to a `private` document without authentication', (done) => {
+      chai.request(server)
+        .get('/api/documents/3')
+        .end((error, response) => {
+          response.status.should.equal(404);
+          response.body.message.should.equal('document not found');
+          done();
+        });
+    });
+    it('should return `document not found` in request to a `role` document without authentication', (done) => {
+      chai.request(server)
+        .get('/api/documents/2')
+        .end((error, response) => {
+          response.status.should.equal(404);
+          response.body.message.should.equal('document not found');
+          done();
+        });
+    });
+    it('should return `you are not signed in` in attempt to `update` a `role` document without authentication', (done) => {
+      chai.request(server)
+        .put('/api/documents/2')
+        .send({ title: 'My awesome title', body: 'my wonderful document content', ownerID: 1, access: 'public' })
+        .end((error, response) => {
+          response.status.should.equal(401);
+          response.body.message.should.equal('you are not signed in');
+          done();
+        });
+    });
+    it('should return `you are not signed in` in attempt to `update` a `private` document without authentication', (done) => {
+      chai.request(server)
+        .put('/api/documents/3')
+        .send({ title: 'My awesome title', body: 'my wonderful document content', ownerID: 1, access: 'public' })
+        .end((error, response) => {
+          response.status.should.equal(401);
+          response.body.message.should.equal('you are not signed in');
+          done();
+        });
+    });
+    it('should return `you are not signed in` in attempt to `delete` a `public` document without authentication', (done) => {
+      chai.request(server)
+        .delete('/api/documents/1')
+        .end((error, response) => {
+          response.status.should.equal(401);
+          response.body.message.should.equal('you are not signed in');
+          done();
+        });
+    });
+    it('should return `you are not signed in` in attempt to `delete` a `role` document without authentication', (done) => {
+      chai.request(server)
+        .delete('/api/documents/2')
+        .end((error, response) => {
+          response.status.should.equal(401);
+          response.body.message.should.equal('you are not signed in');
+          done();
+        });
+    });
+    it('should return `you are not signed in` in attempt to `delete` a `private` document without authentication', (done) => {
+      chai.request(server)
+        .delete('/api/documents/3')
+        .end((error, response) => {
+          response.status.should.equal(401);
+          response.body.message.should.equal('you are not signed in');
+          done();
+        });
+    });
+  });
+});
+
