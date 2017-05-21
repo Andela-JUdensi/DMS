bfe402dde8481beb51eed8ad57a0a1dba5741895
andela-judensi <joshua.udensi@andela.com>
2017-05-21 18:47:02 +0100

    feature(reducer): root reducer
    [#145816283]

---
+++ client/reducers/rootReducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import authentication from './authentication.reducer';
+import documents from './documents.reducer';
+import search from './search.reducer';
+import user from './user.reducer';
+import allUsers from './allUsers.reducer';
+
+
+export default combineReducers({
+  authentication,
+  documents,
+  search,
+  user,
+  allUsers
+});
