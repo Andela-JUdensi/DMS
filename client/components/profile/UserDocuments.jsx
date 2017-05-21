a48301b6dfd08745c18ba8cc9768136f659c985e
andela-judensi <joshua.udensi@andela.com>
2017-05-21 18:35:07 +0100

    feature(UserDocuments): component for each user documents
    [#145816283]

---
+++ client/components/profile/UserDocuments.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  withRouter
+} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import CircularProgress from 'material-ui/CircularProgress';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { userDocumentsAction } from '../../actions/users.action';
+import documentStack from '../common/documentsStack';
+
+
+class UserDocuments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props.userDocumentsAction(this.props.match.params.id);
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.props.userDocuments
+          ?
+            <div>
+              <MuiThemeProvider>
+                <div>
+                  <h3>
+                    {this.props.userDocuments.count} Documents
+                  </h3>
+                  {
+                    this.props.userDocuments.count < 1
+                    ?
+                      <CircularProgress size={60} thickness={7} />
+                    :
+                      ''
+                  }
+                </div>
+              </MuiThemeProvider>
+              {
+                this.props.userDocuments.rows.map(documentStack)
+              }
+            </div>
+          :
+            <MuiThemeProvider>
+              <div>
+                <h3>
+                  No document yet
+                </h3>
+                <CircularProgress size={60} thickness={7} />
+              </div>
+            </MuiThemeProvider>
+        }
+      </div>
+    );
+  }
+}
+UserDocuments.propTypes = {
+  userDocuments: PropTypes.object,
+};
+
+const mapStateToProps = state => ({
+  userDocuments: state.user.userDocuments,
+});
+
+export default withRouter(connect(mapStateToProps, { userDocumentsAction })(UserDocuments));
