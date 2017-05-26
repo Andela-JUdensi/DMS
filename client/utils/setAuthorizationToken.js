3161794cc72fdcdb2f7ff8d895f4cd2c38b716fc
andela-judensi <joshua.udensi@andela.com>
2017-05-26 06:52:15 +0100

    feature(client auth): authenticate react/client routes
    [#145816283]

--- client/utils/setAuthorizationToken.js
+++ client/utils/setAuthorizationToken.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const setAuthorizationToken = (token) => {
   const axiosCalls = axios.create();
-  const defaultHeaders = axiosCalls.defaults.headers.common = {};
+  const defaultHeaders = axiosCalls.defaults.headers.common || {};
 
   if (token) {
     defaultHeaders.authorization = `Bearer ${token}`;
