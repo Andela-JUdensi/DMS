c7626141cfc9ec9c167386a182f9c202f5db216c
andela-judensi <joshua.udensi@andela.com>
2017-05-26 06:53:21 +0100

    feature(routes): add middlewares to specific middleware routes
    [#145816283]

--- server/configs/configs/database.config.js
+++ server/configs/configs/database.config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    // logging: false
+    logging: false
   },
   test: {
     use_env_variable: 'DATABASE_URL',
